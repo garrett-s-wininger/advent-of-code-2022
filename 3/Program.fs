dab82a6055e1796eb4203e571ec56527cb4e1422
Garrett Wininger <garrett.s.wininger@outlook.com>
2022-12-09 23:21:59 -0500

    Advent of Code 2022 - Day 3 Challenge

---
+++ 3/Program.fs
@@ -0,0 +1,50 @@
+﻿open System.IO
+
+let readRucksacks (filePath: string) = seq {
+    use sr = new StreamReader(filePath)
+    
+    while not sr.EndOfStream do
+        yield sr.ReadLine ()
+}
+
+let itemToPriority (item: char) =
+    let lowerCaseSubValue = 96
+    let upperCaseSubValue = 38
+
+    if int item >= int 'a' && int item <= int 'z' then int item - lowerCaseSubValue
+    elif int item >= int 'A' && int item <= int 'Z' then int item - upperCaseSubValue
+    else failwith "itemToPriority provided with a value outside the range of a-z or A-Z"
+     
+let matchItems ((left, right): string*string) =
+    List.filter (fun item -> List.contains item (Seq.toList right)) (Seq.toList left) |> Set.ofList
+
+[<EntryPoint>]
+let main args =
+    if args.Length <> 1 then failwith "Usage: dotnet run <filepath>"
+
+    let rucksackCompartments = readRucksacks(args[0]) |> Seq.map (fun ruck ->
+        let splitIdx = (ruck.Length / 2) - 1
+        
+        (ruck[0..splitIdx], ruck[splitIdx + 1..])
+    ) 
+    
+    let sharedItems = rucksackCompartments |> Seq.map (fun compartmentPair ->
+        matchItems(compartmentPair)
+    ) 
+
+    let priorities = sharedItems |> Seq.map (fun sharedItemSet ->
+        sharedItemSet |> Set.map (fun sharedItem ->
+            itemToPriority sharedItem
+        )
+    )
+
+    let prioritySums = priorities |> Seq.map (fun prioritySet ->
+        Set.fold (+) 0 prioritySet
+    )
+
+    let totalSum = Seq.fold (+) 0 prioritySums
+
+    printfn "Total sum: %d" totalSum
+
+    0
+
---
+++ 3/README.md
@@ -0,0 +1,14 @@
+# Advent of Code Day 3 - Rucksack Reorganization
+
+This folder contains the attempted solution to the third of the Advent of 
+Code 2022 programming challenges. The objective here is to take a file 
+containing the items in rucksack compartments and output the priority items 
+which are contained in both the left and right compartements.
+
+You can run the program with the following:
+
+	dotnet run input
+
+As far as tooling is concerned, you will need to download the latest dotnet
+version form Microsoft in order to build and run the project.
+
