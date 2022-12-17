9b720ce55ece3b6b17fece9e696883a23e89a0e0
Garrett Wininger <garrett.s.wininger@outlook.com>
2022-12-17 15:10:20 -0500

    Advent of Code 2022 Day 10 Challenge

---
+++ 10/Program.cs
@@ -0,0 +1,57 @@
+﻿if (args.Length != 1)
+{
+    Console.Error.WriteLine("Usage: dotnet run <filepath>");
+    Environment.Exit(1);
+}
+
+int cycle = 0;
+int register = 1;
+int signal = 0;
+
+bool isMonitoredCycle(int cycle)
+{
+    return (cycle == 20 || ((cycle - 20) % 40 == 0));
+}
+
+void updateSignal(int cycle, int register, ref int signal)
+{
+    if (isMonitoredCycle(cycle))
+    {
+        signal += (cycle * register);
+        Console.WriteLine($"Cycle {cycle}: {register} ({signal})");
+    }
+}
+
+void noop(ref int cycle, ref int signal, int regsiter)
+{
+    ++cycle;
+    updateSignal(cycle, register, ref signal);
+}
+
+void addx(int amount, ref int cycle, ref int signal, ref int register)
+{
+    ++cycle;
+    updateSignal(cycle, register, ref signal);
+    ++cycle;
+    updateSignal(cycle, register, ref signal);
+    register += amount;
+}
+
+using (StreamReader reader = new StreamReader(args[0]))
+{
+    string? line;
+
+    while ((line = reader.ReadLine()) != null)
+    {
+        string[] instructionComponents = line.Split(" ");
+        
+        if (instructionComponents[0] == "noop")
+        {
+            noop(ref cycle, ref signal, register);
+        }
+        else
+        {
+            addx(int.Parse(instructionComponents[1]), ref cycle, ref signal, ref register);
+        }
+    }
+}
---
+++ 10/README.md
@@ -0,0 +1,14 @@
+# Advent of Code 2022 - Day 10 Cathode-Ray Tube
+
+This folder contains the attempted solution to the tenth of the Advent of Code 
+2022 challenges. In this, we want to simulate a CPU containing noop and addx 
+instructions wich take varying cycles to complete. At certain points, we'll 
+take the value of the underlying register and multiply by the cycle count 
+to determine signal strength.
+
+The following command will run the solution:
+
+    dotnet run input
+
+As this is a .NET application, you will need an available version of .NET 
+on your system in order to compile and run the code.
