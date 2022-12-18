473a75e14d56e58df33d1a20777e505f35027aab
Garrett Wininger <garrett.s.wininger@outlook.com>
2022-12-18 18:58:37 -0500

    Advent of Code 2022 Day 12 Challenge

---
+++ 12/README.md
@@ -0,0 +1,13 @@
+# Advent of Code 2022 - Day 12 Hill Climbing Algorithm
+
+This folder contains the attempted solution to the day 12 challenge of the 
+Advent of Code 2022. In this, we want to read in a heightmap of the surrounding 
+area, identify our start and end locations, and then identify a route to the 
+desired location in the shortest number of steps possible.
+
+The program can be run with the following:
+
+    ./main.pl
+
+As the solution is implemented in Perl, a working interpreter for the language 
+should be all you need to run the solution.
---
+++ 12/main.pl
@@ -0,0 +1,93 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use v5.30.0;
+
+use List::Util qw(first);
+
+if ( !( $#ARGV + 1 ) ) {
+    say STDERR "Usage: ./main.pl <filepath>";
+    exit 1;
+}
+
+chomp( my @lines = <> );
+my @content   = unpack "W*", join( "", @lines );
+my $start_idx = first { $content[$_] eq 83 } 0 .. $#content;
+my $dest_idx  = first { $content[$_] eq 69 } 0 .. $#content;
+my $width     = length $lines[0];
+
+my @queue   = ($start_idx);
+my %visited = ();
+my %parents = ();
+
+sub can_move {
+    my $array_ref   = shift;
+    my $source      = shift;
+    my $destination = ${$array_ref}[shift];
+
+    if ( $destination == 69 ) {
+        $destination = 122;
+    }
+
+    return $destination <= ${$array_ref}[$source] + 1;
+}
+
+sub in_bounds {
+    my $array_ref = shift;
+    my $idx       = shift;
+
+    return $idx > 0 && $idx < scalar @{$array_ref};
+}
+
+sub same_grid_line {
+    my $width       = shift;
+    my $source      = shift;
+    my $destination = shift;
+
+    return ( int $source / $width ) == ( int $destination / $width );
+}
+
+while ( scalar @queue > 0 ) {
+    my $idx = shift @queue;
+
+    if ( exists $visited{$idx} ) {
+        next;
+    }
+
+    $visited{$idx} = 1;
+    my $up_idx    = $idx - $width;
+    my $down_idx  = $idx + $width;
+    my $left_idx  = $idx - 1;
+    my $right_idx = $idx + 1;
+
+    foreach ( $up_idx, $down_idx, $left_idx, $right_idx ) {
+        if ( in_bounds( \@content, $_ ) && !exists $visited{$_} ) {
+            if ( ( $_ == $left_idx || $_ == $right_idx )
+                && !same_grid_line( $width, $idx, $_ ) )
+            {
+                next;
+            }
+
+            $parents{$_} = $idx;
+
+            if ( $content[$idx] >= 121 && $_ == $dest_idx ) {
+                $parents{$dest_idx} = $idx;
+                last;
+            }
+            elsif ( scalar %visited == 1 || can_move( \@content, $idx, $_ ) ) {
+                push @queue, $_;
+            }
+        }
+    }
+}
+
+my $move_count = 0;
+my $path_idx   = $dest_idx;
+
+while ( exists ${ parents { $path_idx } } ) {
+    ++$move_count;
+    $path_idx = ${ parents { $path_idx } };
+}
+
+print "Reached destination after $move_count moves\n";
